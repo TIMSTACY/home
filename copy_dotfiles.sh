fb23aa639bbabdc85825b5793301a75da2bd670b
timstacy <tim.stacy@tradingtechnologies.com>
2017-11-07 08:28:47 -0600

    more updastes

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -7,8 +7,8 @@ cp -v  ~/.viminfo ~/home_repo/dotfiles/
 cp -v  ~/.vimrc ~/home_repo/dotfiles/
 cp -v  ~/.vimrc_background ~/home_repo/dotfiles/
 
-cp -v  ~/.deploy_notes ~/home_repo/misc/
-cp -v  ~/.newrepo_notes ~/home_repo/misc/
-cp -v  ~/.notes ~/home_repo/misc/
-cp -v  ~/.tmux_notes ~/home_repo/misc/
-cp -v  ~/.transfer_cookbook_notes ~/home_repo/misc/
+cp -v  ~/deploy_notes ~/home_repo/misc/
+cp -v  ~/newrepo_notes ~/home_repo/misc/
+cp -v  ~/notes ~/home_repo/misc/
+cp -v  ~/tmux_notes ~/home_repo/misc/
+cp -v  ~/transfer_cookbooks_notes ~/home_repo/misc/
